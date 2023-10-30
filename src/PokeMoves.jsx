b6800204d9461faf6760696798361579ca3c021a
Zhongyuan Ding <zhongyuan.d@gmail.com>
2023-10-30 16:13:28 -0400

    solved

--- src/PokeMoves.jsx
+++ src/PokeMoves.jsx
@@ -1,19 +1,18 @@
-import "./PokeMoveCard.css";
-import { moves } from "./data"
+import { PokeMoveCard } from "./PokeMoveCard"
+import moves from "./data";
 
 const PokeMoves = () => {
     return (
-        <div>
-            <h1>PokeMoves</h1>
-            <ul>
-                {moves.map(item => (
-                    <li>
-                        {item.id}. {item.move}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+      <div>
+        <h1>PokeMoves</h1>
+        <ul>
+          {moves.map(item => (
+            //for each item, create a li with key = item.id and props destructed from the item.params
+            <PokeMoveCard key={item.id} {...item} />
+          ))}
+        </ul>
+      </div>
+    );
+  };
 
-export default PokeMoves;
+  export default PokeMoves;
